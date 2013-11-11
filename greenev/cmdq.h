b51cf4f27a8bd20ec9009b3890472ecaa558da5c
Ales Teska <ales.teska@gmail.com>
2013-11-11 23:46:30 +0100

    Preparing declaration of conditional variable based command queue.

--- greenev/cmdq.h
+++ greenev/cmdq.h
@@ -39,5 +39,39 @@ void watcher_cmd_q_stop(struct watcher_cmd_q *); /* Is also implicty called by d
 
 // This method is callable from other threads
 bool watcher_cmd_q_insert(struct watcher_cmd_q * this, int cmd_id, void * arg);
+///
+
+struct cond_cmd_q
+/*
+Command queue based on Posix conditional variable.
+
+Synchronization is done purely via Posix condition variables.
+Therefore this is the only element that can feed thread event loop.
+*/
+{
+	unsigned int q_size;
+	unsigned int q_head;
+	unsigned int q_tail;
+
+	pthread_mutex_t q_mtx;
+	pthread_cond_t q_not_full;
+	pthread_cond_t q_not_empty;
+
+	struct cmd q[];
+};
+
+struct cond_cmd_q * cond_cmd_q_new(unsigned int size);
+void cond_cmd_q_delete(struct cond_cmd_q *);
+
+unsigned int cond_cmd_q_size(struct cond_cmd_q *);
+bool cond_cmd_q_empty(struct cond_cmd_q *);
+bool cond_cmd_q_full(struct cond_cmd_q *);
+
+bool cond_cmd_q_put(struct cond_cmd_q *, int cmd_id, void * arg);
+bool cond_cmd_q_timed_put(struct cond_cmd_q *, int cmd_id, void * arg, unsigned int timeout_ms);
+
+bool cond_cmd_q_get(struct cond_cmd_q *, struct cmd * cmd_out);
+bool cond_cmd_q_timed_get(struct cond_cmd_q *, struct cmd * cmd_out, unsigned int timeout_ms);
+
 
 #endif //GREENEV_CMDQ_H_
